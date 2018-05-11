4913bfbe73836d850ec628577fa8c1fb5b6ac97e
Sergey Lenkov <lenkov@lenkov.work>
2018-05-12 00:26:25 +0300

    Filling Table column and writing log

--- cern_to_altvault.py
+++ cern_to_altvault.py
@@ -1,14 +1,29 @@
 import os
 import re
 import pyodbc
+from datetime import datetime
 
 # User
 import DBstructure
 import config
 
-# TODO: (!) Insert 'Table' column
 # TODO: generate query with .format()
-# TODO: write log
+
+### Create log file ###
+nowTimeStamp = datetime.now().strftime("%Y-%m-%d %H.%M.%S")
+logFileName = os.path.dirname(config.DB_path) + '\\Transfer ' + nowTimeStamp + '.log'
+
+try:
+    logFile = open(logFileName, "w")
+except Exception as e:
+    print("ERROR opening log file: {}".format(e))
+else:
+    print("{} log file opened successfully".format(logFile.name))
+
+msg = 'Date/Time: {}\nTarget DB: {}\nDonor CERN DBs: {}\n\n'.format(nowTimeStamp, config.DB_path, config.CERNdir)
+logFile.write(msg)
+
+print('Processing...')
 
 # Connect to target single table Database
 targetCon = pyodbc.connect('DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ='+config.DB_path)
@@ -21,9 +36,17 @@ for fileName in os.listdir(config.CERNdir):
     if re.search(r'.mdb', fileName):
         DBs.append(config.CERNdir + '\\' + fileName)
 
+# Statistics counters
+totalDBs = 0
+totalTables = 0
+totalRecords = 0
+
 # Perfor operation for each donor Database
 for DB_path in DBs:
-    print('====== Database =======\n{}\n'.format(DB_path))
+    # Statistics
+    totalDBs += 1
+
+    logFile.write('====== Database =======\n{}\n\n'.format(DB_path))
     donorCon = pyodbc.connect('DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ='+DB_path)
     donorCur = donorCon.cursor()
 
@@ -35,11 +58,15 @@ for DB_path in DBs:
 
 
     for table in needTables:
-        print('-------- Table {} ---------\n'.format(table))
+        # Statistics
+        tableRecords = 0
+        totalTables += 1
+
+        logFile.write('-------- Table {} ---------\n\n'.format(table))
 
         query = 'SELECT * FROM `'
         query += table    
-        query += '` WHERE CreateDate>=#01/01/2017#'
+        query += '`'
 
         donorCur.execute(query)
         parts = donorCur.fetchall()
@@ -48,7 +75,7 @@ for DB_path in DBs:
             targetCur.execute(targetSearch, part[0])
             targetResult = targetCur.fetchall()
             if not targetResult:
-                print('Transfer part:\n{}\n'.format(part))
+                logFile.write('Transfer part:\n{}\n\n'.format(part))
 
                 # Obtain match columns and assembly INSERT query
                 colSkeleton = ''
@@ -68,30 +95,52 @@ for DB_path in DBs:
                 donorSelect += ' FROM `'
                 donorSelect += table
                 donorSelect += '` WHERE `Part Number` LIKE ?'
-                print('Assembled SELECT query for donor Database:\n{}\n'.format(donorSelect))
+                logFile.write('Assembled SELECT query for donor Database:\n{}\n\n'.format(donorSelect))
 
                 targetInsert = 'INSERT INTO components ('
-                targetInsert += colSkeleton[:-2]
-                targetInsert += ') VALUES ('
-                targetInsert += ('?,'*colCount)[:-1]
-                targetInsert += ')'
-                print('Assembled INSERT query for target Database:\n{}\n'.format(targetInsert))
+                targetInsert += colSkeleton
+                targetInsert += '`Table`) VALUES ('
+                targetInsert += '?,'*colCount
+                targetInsert += '?)'
+                logFile.write('Assembled INSERT query for target Database:\n{}\n\n'.format(targetInsert))
 
-                print('-----------------------------\n')
+                logFile.write('-----------------------------\n\n')
                 
                 donorCur.execute(donorSelect, part[0])
                 donorResult = donorCur.fetchall()
+                insertData = list(donorResult[0])
 
                 # Change Library and Footprint Path (move to 'CERN\' directory)
                 if libPathIdx != 0:
-                    donorResult[0][libPathIdx] = 'CERN\\' + donorResult[0][libPathIdx]                    
+                    insertData[libPathIdx] = 'CERN\\' + insertData[libPathIdx]                    
                 if footPathIdx != 0:
-                    donorResult[0][footPathIdx] = 'CERN\\' + donorResult[0][footPathIdx]
+                    insertData[footPathIdx] = 'CERN\\' + insertData[footPathIdx]
+
+                # Add Table information
+                insertData.append(table)
                 
-                targetCur.execute(targetInsert, donorResult[0])
+                targetCur.execute(targetInsert, insertData)
                 targetCur.commit()
+
+                # Statistics
+                tableRecords += 1
+                totalRecords += 1
+        
+        logFile.write('*************************************\n')
+        logFile.write('Transfered records in this table: {}\n'.format(tableRecords))
+        logFile.write('*************************************\n\n\n')
 
     donorCon.close()
 
-targetCon.close()
+# Statistics
+logFile.write('\n\n\n*******Total Statistics*******\n')
+logFile.write('Total donor DBs: {}\n'.format(totalDBs))
+logFile.write('Total read tables: {}\n'.format(totalTables))
+logFile.write('Total transfered records: {}\n'.format(totalRecords))
+logFile.write('******************************\n')
 
+print('Transfer successful')
+print('See log in {}'.format(logFileName))
+
+targetCon.close()
+logFile.close()
