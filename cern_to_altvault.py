22849fc4d2463222d65e4bc56c82f3d5a8a48d47
Sergey Lenkov <lenkov@lenkov.work>
2018-05-12 08:18:29 +0300

    Added try checks and some corrections

--- cern_to_altvault.py
+++ cern_to_altvault.py
@@ -1,9 +1,11 @@
 import os
+import sys
 import re
 import pyodbc
 from datetime import datetime
 
 # User
+from markup import *
 import DBstructure
 import config
 
@@ -13,7 +15,7 @@ nowTimeStamp = datetime.now().strftime("%Y-%m-%d %H.%M.%S")
 logFileName = os.path.dirname(config.DB_path) + '\\Transfer ' + nowTimeStamp + '.log'
 
 try:
-    logFile = open(logFileName, "w")
+    logFile = open(logFileName, "w", encoding='utf-8')
 except Exception as e:
     print("ERROR opening log file: {}".format(e))
 else:
@@ -25,8 +27,15 @@ logFile.write(msg)
 print('Processing...')
 
 # Connect to target single table Database
-targetCon = pyodbc.connect('DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ='+config.DB_path)
-targetCur = targetCon.cursor()
+try:
+    targetCon = pyodbc.connect('DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ='+config.DB_path)
+    targetCur = targetCon.cursor()
+    targetCur.execute("SELECT `Part Number` FROM components")
+except:
+    errprint("Wrong target database")
+    input("Type any key for exit")
+    sys.exit(0)
+    
 targetSearch = 'SELECT `Part Number` FROM components WHERE `Part Number` LIKE ?'
 
 # Obtain full paths for donor Databases
@@ -34,6 +43,11 @@ DBs = []
 for fileName in os.listdir(config.CERNdir):
     if re.search(r'.mdb', fileName):
         DBs.append(config.CERNdir + '\\' + fileName)
+
+if not DBs:
+    errprint("Not found donor Database")
+    input("Type any key for exit")
+    sys.exit(0)
 
 # Statistics counters
 totalDBs = 0
@@ -46,85 +60,92 @@ for DB_path in DBs:
     totalDBs += 1
 
     logFile.write('====== Database =======\n{}\n\n'.format(DB_path))
-    donorCon = pyodbc.connect('DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ='+DB_path)
-    donorCur = donorCon.cursor()
 
-    # Obtain necessary tables in DB
-    needTables = []
-    for col in donorCur.columns():
-        if col.column_name == 'Part Number':
-            needTables.append(col.table_name)
+    try:
+        donorCon = pyodbc.connect('DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ='+DB_path)
+        donorCur = donorCon.cursor()
+    except:
+        logFile.write('Cannot connect to this Database\n')
+    else:
+        # Obtain necessary tables in DB
+        needTables = []
+        for col in donorCur.columns():
+            if col.column_name == 'Part Number':
+                needTables.append(col.table_name)
 
-
-    for table in needTables:
-        # Statistics
-        tableRecords = 0
-        totalTables += 1
-
-        logFile.write('-------- Table {} ---------\n\n'.format(table))
-
-        query = 'SELECT * FROM `'
-        query += table    
-        query += '`'
-
-        donorCur.execute(query)
-        parts = donorCur.fetchall()
-
-        for part in parts:
-            targetCur.execute(targetSearch, part[0])
-            targetResult = targetCur.fetchall()
-            if not targetResult:
-                logFile.write('Transfer part:\n{}\n\n'.format(part))
-
-                # Obtain match columns and assembly INSERT query
-                colSkeleton = ''
-                colCount = 0
-                libPathIdx = 0
-                footPathIdx = 0
-
-                for col in donorCur.columns(table=table):
-                    if col.column_name in DBstructure.colNames:
-                        colSkeleton += '`' + col.column_name + '`, '
-                        libPathIdx = colCount if col.column_name == 'Library Path' else libPathIdx
-                        footPathIdx = colCount if col.column_name == 'Footprint Path' else footPathIdx
-                        colCount += 1
-
-                donorSelect = 'SELECT {} FROM `{}` WHERE `Part Number` LIKE ?'.format(colSkeleton[:-2], table)
-                logFile.write('Assembled SELECT query for donor Database:\n{}\n\n'.format(donorSelect))
-
-                targetInsert = 'INSERT INTO components ({}`Table`) VALUES ({}?)'.format(colSkeleton, '?,'*colCount)
-                logFile.write('Assembled INSERT query for target Database:\n{}\n\n'.format(targetInsert))
-
-                logFile.write('-----------------------------\n\n')
-                
-                donorCur.execute(donorSelect, part[0])
-                donorResult = donorCur.fetchall()
-                insertData = list(donorResult[0])
-
-                # Change Library and Footprint Path (move to 'CERN\' directory)
-                if libPathIdx != 0:
-                    insertData[libPathIdx] = 'CERN\\' + insertData[libPathIdx]                    
-                if footPathIdx != 0:
-                    insertData[footPathIdx] = 'CERN\\' + insertData[footPathIdx]
-
-                # Add Table information
-                insertData.append(table)
-                
-                targetCur.execute(targetInsert, insertData)
-                targetCur.commit()
-
+        if needTables:
+            for table in needTables:
                 # Statistics
-                tableRecords += 1
-                totalRecords += 1
-        
-        logFile.write('*************************************\n')
-        logFile.write('Transfered records in this table: {}\n'.format(tableRecords))
-        logFile.write('*************************************\n\n\n')
+                tableRecords = 0
+                totalTables += 1
 
-    donorCon.close()
+                logFile.write('-------- Table {} ---------\n\n'.format(table))
+                query = 'SELECT * FROM `{}`'.format(table)
+                # query = 'SELECT * FROM `{}` WHERE CreateDate>=#01/01/2017#'.format(table)
+                donorCur.execute(query)
+                parts = donorCur.fetchall()
+
+                for part in parts:
+                    targetCur.execute(targetSearch, part[0])
+                    targetResult = targetCur.fetchall()
+                    if not targetResult: # If the part is not in the database yet
+                        logFile.write('Transfer part:\n{}\n\n'.format(part))
+
+                        # Obtain match columns and assembly INSERT query
+                        colSkeleton = ''
+                        colCount = 0
+                        libPathIdx = 0
+                        footPathIdx = 0
+
+                        for col in donorCur.columns(table=table):
+                            if col.column_name in DBstructure.colNames:
+                                colSkeleton += '`' + col.column_name + '`, '
+                                libPathIdx = colCount if col.column_name == 'Library Path' else libPathIdx
+                                footPathIdx = colCount if col.column_name == 'Footprint Path' else footPathIdx
+                                colCount += 1
+
+                        donorSelect = 'SELECT {} FROM `{}` WHERE `Part Number` LIKE ?'.format(colSkeleton[:-2], table)
+                        logFile.write('Assembled SELECT query for donor Database:\n{}\n\n'.format(donorSelect))
+
+                        targetInsert = 'INSERT INTO components ({}`Table`) VALUES ({}?)'.format(colSkeleton, '?,'*colCount)
+                        logFile.write('Assembled INSERT query for target Database:\n{}\n\n'.format(targetInsert))
+
+                        try:                            
+                            donorCur.execute(donorSelect, part[0])
+                            donorResult = donorCur.fetchall()
+                            insertData = list(donorResult[0])
+
+                            # Change Library and Footprint Path (move to 'CERN\' directory)
+                            if libPathIdx != 0:
+                                insertData[libPathIdx] = 'CERN\\' + insertData[libPathIdx]                    
+                            if footPathIdx != 0:
+                                insertData[footPathIdx] = 'CERN\\' + insertData[footPathIdx]
+
+                            # Add Table information
+                            insertData.append(table)
+                            
+                            targetCur.execute(targetInsert, insertData)
+                            targetCur.commit()
+                        except Exception as e:
+                            logFile.write('Cannot transfer part. {}\n'.format(e))
+                        else:
+                            logFile.write('Transfer successful\n\n')
+                            # Statistics
+                            tableRecords += 1
+                            totalRecords += 1
+
+                        logFile.write('-----------------------------\n\n')
+                
+                logFile.write('*************************************\n')
+                logFile.write('Transfered records in this table: {}\n'.format(tableRecords))
+                logFile.write('*************************************\n\n\n')
+        else:
+            logFile.write('Not found valid tables in Database')
+
+        donorCon.close()
 
 # Statistics
-logFile.write('\n\n\n*******Total Statistics*******\n')
+logFile.write('\n\n\n****** Total Statistics ******\n')
 logFile.write('Total donor DBs: {}\n'.format(totalDBs))
 logFile.write('Total read tables: {}\n'.format(totalTables))
 logFile.write('Total transfered records: {}\n'.format(totalRecords))
